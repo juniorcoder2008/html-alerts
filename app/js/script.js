e16f7e0447c2e50ec237e14ccc14287bb319790a
JuniorCoder <57005033+juniorcoder2008@users.noreply.github.com>
2022-02-10 16:43:06 +0100

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+// Colors
+const colors = {
+    main: '#568FF0',
+    darkMain: '#2F3137',
+    blackMain: '#191A1D',
+    green: '#27FFA9',
+    red: '#FF0077',
+    yellow: '#FFCC00',
+    white: '#eeeeee',
+    black: '#1b1b1b',
+    fullWhite: '#fff',
+    fullBlack: '#000',
+};
