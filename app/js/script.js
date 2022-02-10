99237ab5ea7a6de2a731e9eef76a89a63c52cf55
JuniorCoder <thomas08samuel@gmail.com>
2022-02-10 16:47:00 +0100

    finish

--- app/js/script.js
+++ app/js/script.js
@@ -11,3 +11,83 @@ const colors = {
     fullWhite: '#fff',
     fullBlack: '#000',
 };
+
+class HTMLAlert {
+    constructor(text) {
+        this.text = text;
+        this.canShow = false;
+    }
+
+    createAlert() {
+        if (!document.querySelector('.alert-container').innerHTML == '') {
+            console.error(
+                'It is not possible to display multiple HTMLAlerts at the same time.'
+            );
+        } else {
+            const container = document.createElement('div');
+            container.classList.add('alert');
+
+            const text = document.createElement('p');
+            text.innerText = this.text;
+
+            const closeButton = document.createElement('div');
+            closeButton.classList.add('close');
+
+            const lineOne = document.createElement('div');
+            lineOne.classList.add('line01');
+            const lineTwo = document.createElement('div');
+            lineTwo.classList.add('line02');
+
+            closeButton.appendChild(lineOne);
+            closeButton.appendChild(lineTwo);
+
+            closeButton.addEventListener('click', () => {
+                gsap.to(document.querySelector('.alert-container .alert'), {
+                    y: -100,
+                    opacity: 0,
+                });
+
+                setTimeout(() => {
+                    document.querySelector('.alert-container .alert').remove();
+                }, 500);
+
+                console.info(
+                    '%c Alert was deleted succesfully!',
+                    `color: ${colors.red}`
+                );
+            });
+
+            container.append(text);
+            container.append(closeButton);
+
+            document.querySelector('.alert-container').appendChild(container);
+
+            console.info(
+                '%c Alert was created succesfully!',
+                `color: ${colors.green}`
+            );
+
+            this.canShow = true;
+        }
+    }
+
+    showAlert() {
+        if (this.canShow) {
+            this.displayAlertTimeline = gsap.timeline();
+            this.displayAlertTimeline.from(
+                document.querySelector('.alert-container .alert'),
+                { y: -100, opacity: 0 }
+            );
+        }
+    }
+}
+
+const createTestAlertBTN = document.getElementById('create-test-alert');
+
+createTestAlertBTN.onclick = () => {
+    const testAlert = new HTMLAlert(
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, consequuntur. Dolorum cumque alias, labore iste sunt quas in quidem! Quis, eaque? Ex dolore molestias exercitationem minima provident recusandae eaque quas laboriosam cum. Illo, repellendus! Tempora blanditiis laborum quidem. Officiis, similique.'
+    );
+    testAlert.createAlert();
+    testAlert.showAlert();
+};
